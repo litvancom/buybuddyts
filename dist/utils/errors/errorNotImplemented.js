f490d890f00f66b15d597801e42b1f4e72232401
Ivan Litovchenko <ivan.litovchenko@onlini.co>
2018-11-25 18:22:35 +0200

    [imp] Unauthenticated error

--- dist/utils/errors/errorNotImplemented.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ErrorNotImplemented extends Error {
-    constructor(message) {
-        super(message || "Not Implemented");
-    }
-}
-exports.ErrorNotImplemented = ErrorNotImplemented;
-//# sourceMappingURL=errorNotImplemented.js.map
--- src/utils/errors/errorNotImplemented.ts
+++ src/utils/errors/errorNotImplemented.ts
@@ -3,3 +3,5 @@ export class ErrorNotImplemented extends Error {
     super(message || "Not Implemented");
   }
 }
+
+
