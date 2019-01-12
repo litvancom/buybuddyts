6f90e36439232e012c849d5326f78175e241030f
Ivan Litovchenko <ivan.litovchenko@onlini.co>
2019-01-12 15:29:09 +0200

    remote schema

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { GraphQLModule } from "@graphql-modules/core";
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer, introspectSchema, makeRemoteExecutableSchema, mergeSchemas } from "apollo-server-express";
 import express = require("express");
+import { HttpLink } from "apollo-link-http";
+import fetch from "node-fetch";
 import config from "./config";
 import { UserModule } from "./graphql/user-module";
 import { logger } from "./utils/logger";
@@ -8,24 +10,40 @@ import { ListModule } from "./graphql/list-module";
 import { SharedListModule } from "./graphql/sharedList-module";
 import { ListItemModule } from "./graphql/listItem-module";
 
-const appModule = new GraphQLModule({
-  imports: [UserModule, ListModule, ListItemModule, SharedListModule]
+// @ts-ignore
+const link = new HttpLink({ uri: "http://api.githunt.com/graphql", fetch });
+
+setImmediate(async () => {
+  const remoteSchema = await introspectSchema(link);
+
+  const executableSchema = makeRemoteExecutableSchema({
+    schema: remoteSchema,
+    link
+  });
+
+  const appModule = new GraphQLModule({
+    imports: [UserModule, ListModule, ListItemModule, SharedListModule]
+  });
+
+  const { schema, context } = appModule;
+
+  const mergedSchema = mergeSchemas({
+    schemas: [schema, executableSchema]
+  });
+
+  const server = new ApolloServer({
+    schema: mergedSchema,
+    context,
+    introspection: true
+  });
+
+  const cors = {
+    origin: "*",
+    credentials: true
+  };
+
+  const app = express();
+  server.applyMiddleware({ app, cors });
+
+  app.listen(config.port, () => logger.info(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
 });
-
-const { schema, context } = appModule;
-
-const server = new ApolloServer({
-  schema,
-  context,
-  introspection: true
-});
-
-const cors = {
-  origin: "*",
-  credentials: true
-};
-
-const app = express();
-server.applyMiddleware({ app, cors });
-
-app.listen(config.port, () => logger.info(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
